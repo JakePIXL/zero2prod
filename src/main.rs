99039d39dfd9c3a7e058c24928aae3fe5e9029de
pixl <jakewjevans@gmail.com>
2023-01-01 23:40:38 -0500

    chapter 4: telemetry

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 use std::net::TcpListener;
 
-use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+
+use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let connetion_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
     let address = format!("127.0.0.1:{}", configuration.application_port);
+
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+
+    run(listener, connetion_pool)?.await
 }
