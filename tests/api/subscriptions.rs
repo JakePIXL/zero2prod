4f4a302381f03232555a8c520a3611d503d01c0a
pixl <jakewjevans@gmail.com>
2023-01-06 14:11:10 -0500

    updated tests & applicaiton chapter 7.2

---
+++ tests/api/subscriptions.rs
@@ -0,0 +1,64 @@
+use crate::helpers::spawn_app;
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app = spawn_app().await;
+
+    let body = "name=jake%20evans&email=jake%40valink.io";
+
+    let response = app.post_subscriptions(body.into()).await;
+
+    assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "jake@valink.io");
+    assert_eq!(saved.name, "jake evans");
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let app = spawn_app().await;
+
+    let test_cases = vec![
+        ("name=jake%20evans", "missing the email"),
+        ("email=jake%40valink.io", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = app.post_subscriptions(invalid_body.into()).await;
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_fields_are_present_but_empty() {
+    let app = spawn_app().await;
+
+    let test_cases = vec![
+        ("name=&email=jake%40valink.io", "empty name"),
+        ("name=jake%20evans&email=", "empty email"),
+        ("name=jake%20evans&email=not-an-email", "invalid email"),
+    ];
+
+    for (body, description) in test_cases {
+        let response = app.post_subscriptions(body.into()).await;
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not return a 400 Bad Request when the payload was {}.",
+            description
+        );
+    }
+}
